84d13df10eaecbcc9ef3e91e47a0d7c4a6d5b51a
AjayYadavAi <proajayyadav0525@gmail.com>
2020-05-26 08:08:32 +0530

    working

--- chat-client.py
+++ chat-client.py
@@ -1,29 +1,12 @@
-import socket 
-import select 
-import sys 
-  
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+import socket
+host = socket.gethostname()
+port = 5420
+s = socket.socket()
 
+s.connect((host,port))
+message = str(5)+','+str(6)
 
-if len(sys.argv) != 3: 
-    print("Correct usage: script, IP address, port number")
-    exit() 
-IP_address = str(sys.argv[1]) 
-Port = int(sys.argv[2]) 
-server.connect((IP_address, Port)) 
-  
-while True: 
-    sockets_list = [sys.stdin, server] 
-    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
-  
-    for socks in read_sockets: 
-        if socks == server: 
-            message = socks.recv(2048) 
-            print(message)
-        else: 
-            message = sys.stdin.readline() 
-            server.send(b'message') 
-            sys.stdout.write("<You>") 
-            sys.stdout.write(message) 
-            sys.stdout.flush() 
-server.close()
+s.sendall(message.encode('utf-8'))
+data = s.recv(1024)
+print(data)
+s.close()
--- chat-server.py
+++ chat-server.py
@@ -1,57 +1,22 @@
 import socket
-import select
-from _thread import *
-import sys
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+host = socket.gethostname()
+port = 5420
+s = socket.socket()
+s.bind((host,port))
+s.listen(3)
+
+print('wating..')
+conn,addr = s.accept()
+print("Connected : ", addr)
+
+while True:
+	data = conn.recv(1024)
+	data = data.decode('utf-8')
+	d = str(sum([ int(x) for x in data.split(',')]))
 
 
+	if not data: break
+	conn.sendall(d.encode('utf-8'))
 
-if len(sys.argv) != 3: 
-    print("Correct usage: script, IP address, port number")
-    exit() 
-  
-IP_address = str(sys.argv[1]) 
-Port = int(sys.argv[2]) 
-server.bind((IP_address, Port))
-server.listen(100) 
-list_of_clients = [] 
-def clientthread(conn, addr): 
-    conn.send(b"Welcome to this chatroom!") 
-    while True: 
-            try: 
-                message = conn.recv(2048) 
-                if message: 
-                    print("<" + addr[0] + "> " + message )
-                    message_to_send = "<" + addr[0] + "> " + message 
-                    broadcast(message_to_send, conn) 
-  
-                else: 
-                    remove(conn) 
- 
-            except: 
-                continue
-  
-def broadcast(message, connection): 
-    for clients in list_of_clients: 
-        if clients!=connection: 
-            try: 
-                clients.send(message) 
-            except: 
-                clients.close() 
- 
-                remove(clients) 
-
-def remove(connection): 
-    if connection in list_of_clients: 
-        list_of_clients.remove(connection) 
-  
-while True: 
-    conn, addr = server.accept() 
-    list_of_clients.append(conn)
-    print(addr[0] + " connected")
-    start_new_thread(clientthread,(conn,addr))     
-  
-conn.close() 
-server.close() 
+conn.close()
