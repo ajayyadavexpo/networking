dab810a84dae0ebb00f8cd3edce3216aea1c2087
AjayYadavAi <proajayyadav0525@gmail.com>
2020-05-29 05:37:28 +0530

    added

--- chat/chat-server.py
+++ chat/chat-server.py
@@ -9,12 +9,16 @@ print("Wating ... ")
 conn,addr = s.accept()
 print("Connected : ",addr)
 
-
-
 while True:
 	data = conn.recv(1024)
 	data = data.decode("UTF-8")
-	print(data)
+	if not data:
+		break
+	else:
+		print(data)
 	conn.sendall(data.encode("UTF-8"))
 
+
+
 conn.close()
+	
